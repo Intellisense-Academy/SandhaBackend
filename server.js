ab0a90600fd6b17801faa0e61439974f62cd89af
mukeshmr0728 <mukeshmr0728@gmail.com>
2025-12-13 12:44:37 +0530

    Completed prisma

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Routes
+import authRoutes from './routes/auth.js';
+
+app.use('/', authRoutes);
 
 app.get("/api", (req, res) => {    
     res.status(200).json({
@@ -13,7 +20,7 @@ app.get("/api", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
